3b944cfa5910b177c5906c35197da1ae1623d7cf
shraddhah209 <shraddhaholmukhe96@gmail.com>
2017-04-07 20:47:39 +0530

    used package for apriori

--- apriori/urls.py
+++ apriori/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from . import views
+from apriori import views
 
 urlpatterns = [
     url(r'^index/$', views.index, name='index'),
--- apriori/views.py
+++ apriori/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from apriori.apyori import apriori
 # Create your views here.
 
 
 def index(request):
     html = "hi"
+    transactions = [
+        ['beer', 'nuts'],
+        ['beer', 'cheese'],
+        ['beer','nuts','milk'],
+        ['beer','nuts','coco'],
+    ]
+    results = list(apriori(transactions))
+    html += str(results)
     return HttpResponse(html)
