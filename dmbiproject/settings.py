4c7b99c9d580d1e2bd8e7b563bcf003320fc5310
shraddhah209 <shraddhaholmukhe96@gmail.com>
2017-04-07 20:23:18 +0530

    added urls file for app, one demo function in views.py

--- dmbiproject/settings.py
+++ dmbiproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apriori.apps.AprioriConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
