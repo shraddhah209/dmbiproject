9668dc8b6021b1e4af8ea580643793066fa6fcb9
shraddhah209 <shraddhaholmukhe96@gmail.com>
2017-04-08 17:59:13 +0530

    csv file added

--- apriori/views.py
+++ apriori/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from apriori.apyori import apriori
+
 # Create your views here.
+import pandas as pd
 
-
-def index(request):
+def index2(request):
     html = "hi"
     transactions = [
         ['beer', 'nuts'],
@@ -15,4 +16,19 @@ def index(request):
     ]
     results = list(apriori(transactions))
     html += str(results)
+    return HttpResponse(html)
+
+def index(request):
+    dataset = pd.read_csv('apriori/market.csv', header=None)
+    transactions = []
+    for i in range(0, 7501):
+        print(i)
+        transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+
+    rules = apriori(transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2)
+
+    results = list(rules)
+    myResults = [list(x) for x in results]
+    html = str(myResults)
+
     return HttpResponse(html)
